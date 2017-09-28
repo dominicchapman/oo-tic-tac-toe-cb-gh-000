032372be6555b4f231949030ff7f451813ea8658
Dominic Chapman <dominicchapman@me.com>
2017-09-28 19:53:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,4 +16,76 @@ class TicTacToe
     [2, 4, 6]  # diagonal TR->BL
   ]
 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, character)
+    @board[index] = character
+  end
+
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index)
+  end
+
+  def current_player
+    turn_count(@board) % 2 == 0 ? "X" : "O"
+  end
+
+  def turn_count
+    @board.count{|i| i == "X" || i == "O"}
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.chomp
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player(@board))
+      display_board(@board)
+    else
+      turn(@board)
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combination|
+
+      @board[combination[0]] == @board[combination[1]] &&
+      @board[combination[1]] == @board[combination[2]] &&
+
+      position_taken?(combination[0])
+    end
+  end
+
+  def full?
+    @board.all?{|i| i == "X" || i == "O"}
+  end
+
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || draw? || full?
+  end
+
+  def winner
+    if won?
+      @board[won?[0]]
+    end
+  end
+
 end

0e4066964df022f80bbd5cd91e25299c8cf2533f
Dominic Chapman <dominicchapman@me.com>
2017-09-28 19:46:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,4 +5,15 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
+  WIN_COMBINATIONS = [
+    [0, 1, 2], # horizontal top
+    [3, 4, 5], # horizontal middle
+    [6, 7, 8], # horizontal bottom
+    [0, 3, 6], # vertical left
+    [1, 4, 7], # vertical middle
+    [2, 5, 8], # vertical right
+    [0, 4, 8], # diagonal TL->BR
+    [2, 4, 6]  # diagonal TR->BL
+  ]
+
 end
